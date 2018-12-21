ec07d48aa90418df59088bac669543ef2991bbad
Raul Strappa <r.strappa@alumnos.duoc.cl>
2018-12-21 02:01:08 -0300

    Actualizacion base de datos

--- tecnoActive (1).sql
+++ tecnoActive (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 20-12-2018 a las 06:11:37
--- Versión del servidor: 10.1.32-MariaDB
--- Versión de PHP: 7.2.5
+-- Servidor: localhost
+-- Tiempo de generación: 21-12-2018 a las 06:00:24
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tecnoactive`
+-- Base de datos: `tecnoActive`
 --
 
 -- --------------------------------------------------------
@@ -42,9 +42,7 @@ CREATE TABLE `compra` (
 --
 
 INSERT INTO `compra` (`idCompra`, `idProducto`, `proveedor`, `cantidadPedido`, `fechaInicio`, `fechaTermino`) VALUES
-('JJP-JJ', 1, 'Kokooo', 60, '2018-12-19', '2018-12-22'),
-('Milk-kk', 1, 'tecPro', 43, '2018-12-19', '2018-12-20'),
-('MKV-JG', 1, 'tecPro', 6, '2018-12-18', '2019-02-18');
+('Milk', 2, 'tecPro', 323, '2018-12-29', '2018-12-30');
 
 -- --------------------------------------------------------
 
@@ -55,32 +53,33 @@ INSERT INTO `compra` (`idCompra`, `idProducto`, `proveedor`, `cantidadPedido`, `
 CREATE TABLE `embarque` (
   `idEmbarque` varchar(11) NOT NULL,
   `idCompra` varchar(30) NOT NULL,
+  `cantidad` int(30) NOT NULL,
   `cantContenedores` int(11) NOT NULL,
-  `bl` varchar(30) NOT NULL,
-  `linea` varchar(11) NOT NULL,
-  `motoNave` varchar(30) NOT NULL,
-  `fechaPedido` date NOT NULL,
-  `fechaEntrega` date NOT NULL,
-  `enPuerto` int(1) NOT NULL DEFAULT '0'
+  `bl` varchar(30) DEFAULT NULL,
+  `linea` varchar(11) DEFAULT NULL,
+  `motoNave` varchar(30) DEFAULT NULL,
+  `fechaPedido` date DEFAULT NULL,
+  `fechaEntrega` date DEFAULT NULL,
+  `pSeguro` varchar(30) DEFAULT NULL,
+  `puertoDestino` varchar(3) DEFAULT NULL,
+  `embarcador` varchar(60) DEFAULT NULL,
+  `consignee` varchar(20) DEFAULT NULL,
+  `tMaritimo` int(11) DEFAULT NULL,
+  `gateIn` int(11) DEFAULT NULL,
+  `diasLibres` int(11) DEFAULT NULL,
+  `depositoDevVacio` int(11) DEFAULT NULL,
+  `enPuerto` int(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `embarque`
 --
 
-INSERT INTO `embarque` (`idEmbarque`, `idCompra`, `cantContenedores`, `bl`, `linea`, `motoNave`, `fechaPedido`, `fechaEntrega`, `enPuerto`) VALUES
-('JJP-JJ-1', 'JJP-JJ', 5, 'JJ/II', 'Recta', 'Poseidon', '2018-12-12', '2018-12-30', 0),
-('JJP-JJ-2', 'JJP-JJ', 2, 'JJ/II/P', 'Curva', 'Perla negra', '2018-12-26', '2018-12-23', 0),
-('JJP-JJ-3', 'JJP-JJ', 7, 'KK/PP', 'Pirata', 'Holandes Herrante', '2018-12-19', '2018-12-23', 0),
-('JJP-JJ-4', 'JJP-JJ', 5, 'II', 'OO', 'Comodoro', '2018-12-19', '2019-01-13', 0),
-('JJP-JJ-5', 'JJP-JJ', 90, 'Lp', 'LL', 'nave', '2018-12-21', '2019-01-20', 0),
-('Milk-kk-1', 'Milk-kk', 3, 'blbl', 'White Star', 'asdasd', '2018-11-28', '2018-12-21', 0),
-('Milk-kk-2', 'Milk-kk', 4, 'xzczxc', 'White Star', 'Olimpic', '2018-12-18', '2018-12-21', 0),
-('Milk-kk-3', 'Milk-kk', 2, 'gfdgfd', 'ghgfhg', 'ertret', '2018-12-04', '2018-12-28', 1),
-('Milk-kk-4', 'Milk-kk', 6, 'ghghgfh', 'dfgfdgfd', 'fghgfh', '2018-12-20', '2018-12-29', 0),
-('Milk-kk-5', 'Milk-kk', 7, 'erewrew', 'tertre', 'fhgfg', '2018-12-29', '2019-01-04', 0),
-('MKV-JG-1', 'MKV-JG', 3, 'no/se/que/bl/poner', 'White Star', 'Titanic', '2018-12-09', '2018-12-17', 1),
-('MKV-JG-2', 'MKV-JG', 4, 'ahora/tengo/bl', 'White Star', 'Olimpic', '2018-12-22', '2018-12-30', 1);
+INSERT INTO `embarque` (`idEmbarque`, `idCompra`, `cantidad`, `cantContenedores`, `bl`, `linea`, `motoNave`, `fechaPedido`, `fechaEntrega`, `pSeguro`, `puertoDestino`, `embarcador`, `consignee`, `tMaritimo`, `gateIn`, `diasLibres`, `depositoDevVacio`, `enPuerto`) VALUES
+('Milk-1', 'Milk', 900, 3, 'no/se/que/bl/poner', 'White Star', 'Titanic', '2018-12-21', '2018-12-27', NULL, '', '', '', 0, 0, 0, 0, 1),
+('Milk-2', 'Milk', 500, 3, 'ahora/tengo/bl', 'black star', 'Olimpic', '2018-12-14', '2018-12-30', NULL, '', '', '', 0, 0, 0, 0, 0),
+('Milk-3', 'Milk', 100, 2, 'ferer', 'Sparrow', 'Perla Negra', '0000-00-00', '0000-00-00', NULL, '', '', '', 0, 0, 0, 0, 1),
+('Milk-4', 'Milk', 50, 5, 'quiero/un/bl', 'Barbosa', 'Venganza', '2018-12-22', '2018-12-23', NULL, '', '', '', 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
