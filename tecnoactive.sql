e9cd0512b8b55d171ba0d861dcbbf1dfa665be99
Raul <r.strappa@alumnos.duoc.cl>
2019-01-02 12:15:37 -0300

    Arreglo base de datos

--- tecnoactive.sql
+++ tecnoactive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-01-2019 a las 15:55:59
+-- Tiempo de generación: 02-01-2019 a las 16:13:22
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tecnoactive`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bl`
+--
+
+CREATE TABLE `bl` (
+  `bl` varchar(100) NOT NULL,
+  `idEmbarque` varchar(30) NOT NULL,
+  `internado` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `bl`
+--
+
+INSERT INTO `bl` (`bl`, `idEmbarque`, `internado`) VALUES
+('bl1', 'PDV1/1', 1),
+('bl2', 'PDV1/1', 1),
+('bl3', 'PDV1/2', 1),
+('bl5', 'PDV-45/1', 0),
+('bl89', 'PDV-45/1', 0),
+('bl9', 'PDV-45/2', 0),
+('bl99', 'PDV-45/1', 0);
 
 -- --------------------------------------------------------
 
@@ -44,6 +69,32 @@ CREATE TABLE `compra` (
 INSERT INTO `compra` (`idCompra`, `idProducto`, `proveedor`, `cantidadPedido`, `fechaInicio`, `fechaTermino`) VALUES
 ('PDV-45', 'KK 32', 'Fruna', 100, '2019-01-19', '2019-01-26'),
 ('PDV1', 'AP 40', 'Fruna', 100, '2018-12-20', '2018-12-20');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `despacho`
+--
+
+CREATE TABLE `despacho` (
+  `rutEmisor` varchar(11) NOT NULL,
+  `rutReceptor` int(11) NOT NULL,
+  `tipoDocumento` varchar(40) NOT NULL,
+  `facturaNro` int(11) NOT NULL,
+  `fechaEmision` date NOT NULL,
+  `montoTotal` int(10) NOT NULL,
+  `idProducto` varchar(20) NOT NULL,
+  `cantidadKG` int(11) NOT NULL,
+  `idDespacho` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `despacho`
+--
+
+INSERT INTO `despacho` (`rutEmisor`, `rutReceptor`, `tipoDocumento`, `facturaNro`, `fechaEmision`, `montoTotal`, `idProducto`, `cantidadKG`, `idDespacho`) VALUES
+('17798854-5', 18935801, 'Factura Electronica', 1, '2018-12-22', 9000, 'KC 35', 1000, 2),
+('18935801-6', 17798854, 'Guia de despacho electronica', 12, '2019-01-10', 8787, 'KC 35', 988, 3);
 
 -- --------------------------------------------------------
 
@@ -89,6 +140,34 @@ INSERT INTO `embarque` (`idEmbarque`, `idCompra`, `cantidad`, `cantContenedores`
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `internacion`
+--
+
+CREATE TABLE `internacion` (
+  `idInternacion` int(11) NOT NULL,
+  `nProvision` int(30) NOT NULL,
+  `bl` varchar(30) NOT NULL,
+  `transferido` int(30) NOT NULL,
+  `nIdentDI` varchar(30) NOT NULL,
+  `fechaPagoDI` date NOT NULL,
+  `fa` varchar(30) NOT NULL,
+  `faFile` varchar(100) NOT NULL,
+  `fechaProvision` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `internacion`
+--
+
+INSERT INTO `internacion` (`idInternacion`, `nProvision`, `bl`, `transferido`, `nIdentDI`, `fechaPagoDI`, `fa`, `faFile`, `fechaProvision`) VALUES
+(1, 52, 'bl1', 6000, '84454', '2018-12-28', '2521', 'PDFS/bl1.pdf', '2018-12-28'),
+(3, 55, 'bl3                           ', 2500, '455', '2018-12-29', '544', 'PDFS/bl3                                                              .jpg', '2018-12-28'),
+(4, 343, 'bl2', 5000, '21', '2018-12-28', '55', 'PDFS/bl2.jpg', '2018-12-08'),
+(5, 667, 'bl9', 8000, '878', '2019-01-26', '56534', 'PDFS/bl9.pdf', '2019-01-25');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `producto`
 --
 
@@ -107,6 +186,31 @@ INSERT INTO `producto` (`idProducto`, `descripcion`, `valor`) VALUES
 ('CF 40', 'Chocolatera', 1000),
 ('KC 35', 'Chocolatera', 1000),
 ('KK 32', 'Heladera', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stock`
+--
+
+CREATE TABLE `stock` (
+  `idStock` int(10) NOT NULL,
+  `bl` varchar(10) NOT NULL,
+  `internadas` int(30) NOT NULL DEFAULT '0',
+  `porInternar` int(30) NOT NULL DEFAULT '0',
+  `despachadas` int(30) NOT NULL DEFAULT '0',
+  `stock` int(30) NOT NULL DEFAULT '0',
+  `resolucion` int(30) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `stock`
+--
+
+INSERT INTO `stock` (`idStock`, `bl`, `internadas`, `porInternar`, `despachadas`, `stock`, `resolucion`) VALUES
+(24, 'PDV1/1', 0, 10, 0, 0, 0),
+(25, 'PDV1/2', 0, 20, 0, 0, 0),
+(26, 'PDV1/3', 0, 30, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -132,10 +236,22 @@ INSERT INTO `usuario` (`idUsuario`, `nombre`, `password`) VALUES
 --
 
 --
+-- Indices de la tabla `bl`
+--
+ALTER TABLE `bl`
+  ADD PRIMARY KEY (`bl`);
+
+--
 -- Indices de la tabla `compra`
 --
 ALTER TABLE `compra`
   ADD PRIMARY KEY (`idCompra`);
+
+--
+-- Indices de la tabla `despacho`
+--
+ALTER TABLE `despacho`
+  ADD PRIMARY KEY (`idDespacho`);
 
 --
 -- Indices de la tabla `embarque`
@@ -144,10 +260,22 @@ ALTER TABLE `embarque`
   ADD PRIMARY KEY (`idEmbarque`);
 
 --
+-- Indices de la tabla `internacion`
+--
+ALTER TABLE `internacion`
+  ADD PRIMARY KEY (`idInternacion`);
+
+--
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idStock`);
 
 --
 -- Indices de la tabla `usuario`
@@ -158,6 +286,24 @@ ALTER TABLE `usuario`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `despacho`
+--
+ALTER TABLE `despacho`
+  MODIFY `idDespacho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `internacion`
+--
+ALTER TABLE `internacion`
+  MODIFY `idInternacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idStock` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
