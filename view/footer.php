1ee6788c68dc85848952bfb29fd8d9df0a3a4b67
Raul <r.strappa@alumnos.duoc.cl>
2019-02-11 12:14:02 -0300

    Nuevo checkbox

--- view/footer.php
+++ view/footer.php
@@ -12,7 +12,7 @@
       </div>
       <div class="modal-body">
         ¿Seguro que desea cerrar sesion?
-        
+
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
@@ -40,7 +40,13 @@
           </div>
           <div class="col-md-6">
             <input type="password" name="passActual" id="passActual" value="">
-            <input type="checkbox" onclick="MostrarContraseñaActual()">Ver
+            <input type="checkbox" id="cbx" style="display: none;" onclick="MostrarContraseñaActual()">Ver
+            <label for="cbx" class="check">
+              <svg width="18px" height="18px" viewBox="0 0 18 18">
+                <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                <polyline points="1 9 7 14 15 4"></polyline>
+              </svg>
+            </label>
           </div>
         </div>
         <div class="row" style="margin-top:10px;">
@@ -49,7 +55,13 @@
           </div>
           <div class="col-md-6">
             <input type="password" name="passNueva" id="passNueva" value="">
-            <input type="checkbox" onclick="MostrarContraseñaNueva()">Ver
+            <input type="checkbox" id="cbxx" style="display: none;" onclick="MostrarContraseñaNueva()">Ver
+              <label for="cbxx" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                  <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                  <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+              </label>
           </div>
         </div>
       </div>
